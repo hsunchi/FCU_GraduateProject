346d29b39c50b912031ce807459b368c209118fb
hsunchi <32382354+hsunchi@users.noreply.github.com>
2019-10-04 13:43:07 +0800

    Add files via upload

--- jeibatext.py
+++ jeibatext.py
@@ -16,34 +16,13 @@ with open('C:\\Users\\ASUS\\Desktop\\wordsimilar\\stopWords.txt', 'r') as file:
         data = data.strip()
         stopWords.append(data)
 
-#回傳陣列(詞與出現數量)
-def get_wordsadnum(txt):
-    jieba.load_userdict('C:\\Users\\ASUS\\Desktop\\wordsimilar\\dict.big')
-    key = jieba.analyse.extract_tags(txt, topK=20, withWeight=False, allowPOS=())  
-    seg_list = jieba.cut(txt,cut_all=False)
-    print(seg_list)
-    #doc_list = list(filter(lambda a: a not in stopWords and a != '\n', seg_list))
-    keyremainderWords = list(filter(lambda a: a not in stopWords and a != '\n',key))
-    c = Counter()
-    for x in keyremainderWords:
-        if len(x)>1 and x != '\r\n':
-            c[x] += 1
-    #print('常用词频度统计结果')	
-    sorted(c) 	
-    b = Counter(c).most_common(1)
-    #print(b)
-   # for (k,v) in c.most_common(10):
-        #print('%s%s %s  %d' % ('  '*(5-len(k)), k, '*'*int(v/3), v))
-    return c		
-    #return countkeys
-
 #回傳陣列(詞)
 #提取關鍵詞
 def get_word(txt):
     countkeys = []
     setkeys=[]
     jieba.load_userdict('C:\\Users\\ASUS\\Desktop\\wordsimilar\\dict.big')
-    key = jieba.analyse.extract_tags(txt, topK=20, withWeight=False, allowPOS=())  #提取關鍵字
+    key = jieba.analyse.extract_tags(txt, topK=5, withWeight=False, allowPOS=())  #提取關鍵字
     seg_list = jieba.cut_for_search(txt)                                           #利用jeiba 做斷詞
     doc_list = list(filter(lambda a: a not in stopWords and a != '\n', key)) 
     c = Counter()
@@ -61,29 +40,6 @@ def get_word(txt):
     #print(countkeys)
     return setkeys
 
-#提取所有詞
-def get_all_word(txt):
-    countkeys = []
-    setkeys=[]
-    jieba.load_userdict('C:\\Users\\ASUS\\Desktop\\wordsimilar\\dict.big')
-    seg_list = jieba.cut_for_search(txt)
-    #doc_list = list(filter(lambda a: a not in stopWords and a != '\n', seg_list))
-    doc_list = list(filter(lambda a: a not in stopWords and a != '\n', seg_list))
-    c = Counter()
-    for x in doc_list:
-        if len(x)>1 and x != '\r\n':
-            c[x] += 1
-    #print('常用词频度统计结果')
-    
-    sorted(c) 	
-    b = Counter(c).most_common(1)
-    #print(b)
-    for (k,v) in c.most_common(100):
-        countkeys.append(k)
-        #print('%s%s %s  %d' % ('  '*(5-len(k)), k, '*'*int(v/3), v))
-    setkeys=list(set(countkeys))
-    #print(countkeys)
-    return setkeys
 
 def download_image(url):
     filename = url.split('/')[-1]   # 以斜線分割保留最後一段
