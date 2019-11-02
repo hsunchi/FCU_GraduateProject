7b455d158c315ca53e748edd4a7a74787d0d2abe
D0542337 <32363695+D0542337@users.noreply.github.com>
2019-11-02 17:52:49 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,6 @@
+from keras.models import model_from_json, load_model
+from keras.models import Model, Sequential
+
+model = load_model('model.h5')
+model.load_weights('vgg_face_weights.h5')
+vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
